f3d636e622dc535b37026264d7294ea403885ef8
Asif-newaz <asif.newaz0093@gmail.com>
2024-02-17 01:46:31 +0600

    feat: work with catch all segment

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, AvatarImage } from "../ui/avatar";
+// import { Avatar, AvatarImage } from "../ui/avatar";
 
 type HeaderProps = {
   title: string;
@@ -9,9 +9,9 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <>
       <div>
-        <Avatar>
+        {/* <Avatar>
           <AvatarImage src="/images/dh-logo.png" alt="" />
-        </Avatar>
+        </Avatar> */}
         <h1 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl text-slate-100">
           {title}
         </h1>
