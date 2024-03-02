d39f617611b7d6f0f6037d841999ff9f3e2498ac
Asif-newaz <asif.newaz0093@gmail.com>
2024-03-02 16:55:43 +0600

    feat: work with routing metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,10 @@ interface aboutProps {
   // Define your component's props here
 }
 
+export const metadata = {
+  title: "About codevolution"
+}
+
 const about: React.FC<aboutProps> = (props) => {
   return (
     <div>
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,8 +1,18 @@
-export default function productDetails({
-  params,
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+export default function productDetails({ params }: Props) {
   return (
     <h1 className="text-xl font-bold">
       Details about product {params.productId}
