9ed30601fa6c36c178904dd6244a0f8ca3fba158
Asif-newaz <asif.newaz0093@gmail.com>
2024-06-06 01:00:14 +0600

    initialized new project

--- src/app/%5Flib/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PrivateRoute() {
-  return <h1>You {`can't`} view this in the browser.</h1>;
-}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="hidden">Home Page Route!!</div>;
-}
