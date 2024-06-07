b36789968d4b6655292d4d1fd7ef9a4ee2dbbc4a
Asif-newaz <asif.newaz0093@gmail.com>
2024-06-07 19:50:47 +0600

    work not-found page

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,8 +1,8 @@
-export default function NotFound() {
+export default function NotFounds() {
   return (
     <div className="h-screen bg-black flex justify-center items-center">
       <div className="text-center text-white">
-        <h1 className="text-3xl">Page not found</h1>
+        <h1 className="text-3xl">Review page not found</h1>
         <p>Could not find requested resource</p>
       </div>
     </div>
