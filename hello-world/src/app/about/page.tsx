41e4b6498b2660619cc1acd0180eef2460a84f59
Asif-newaz <asif.newaz0093@gmail.com>
2024-06-08 17:55:56 +0600

    work with title metadata

--- hello-world/src/app/about/page.tsx
+++ hello-world/src/app/about/page.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import { Metadata } from "next";
 
 interface aboutProps {
   // Define your component's props here
 }
 
-export const metadata = {
-  title: "About codevolution"
-}
+export const metadata: Metadata = {
+  title: "About",
+};
 
 const about: React.FC<aboutProps> = (props) => {
   return (
     <div>
       {/* Your component JSX */}
-      <p className='text-2xl'>Welcome about!</p>
+      <p className="text-2xl">Welcome about!</p>
     </div>
   );
-}
+};
 
 export default about;
