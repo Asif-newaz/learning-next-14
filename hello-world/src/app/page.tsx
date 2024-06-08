6eec3e79b776ed8d1ec1639e8a59d5fc774eed57
Asif-newaz <asif.newaz0093@gmail.com>
2024-06-08 23:11:03 +0600

    work link component navigation

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="text-3xl">Welcome home!</div>;
+  return (
+    <>
+      <div className="text-3xl">Welcome home!</div>
+      <Link href="/products">Products</Link>
+      <Link href="/blog">Blog</Link>
+    </>
+  );
 }
